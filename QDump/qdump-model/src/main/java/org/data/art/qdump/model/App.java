acdcc84ce9f0887ea9f3c984adafb6c757e41464
VlasovArtem <vlasovartem21@gmail.com>
2014-11-15 21:19:58 +0200

    Create basic structure of web project.

---
+++ QDump/qdump-model/src/main/java/org/data/art/qdump/model/App.java
@@ -0,0 +1,13 @@
+package org.data.art.qdump.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
